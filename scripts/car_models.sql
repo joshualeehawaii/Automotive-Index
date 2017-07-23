a62bb0f952223136f020aa80d3375849bbc1db72
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-22 20:10:04 -1000

    changes for Nakazs and new times

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS car_models
  PRIMARY KEY (id)
 );
 
-CREATE INDEX car_models_make_code_idx
-ON car_models (make_code);
+-- CREATE INDEX car_models_make_code_idx
+-- ON car_models (make_code);
 
-CREATE INDEX car_models_code_title_idx
-ON car_models (model_code);
+-- CREATE INDEX car_models_code_title_idx
+-- ON car_models (model_code);
 
-CREATE INDEX car_models_year_idx
-ON car_models (year);
+-- CREATE INDEX car_models_year_idx
+-- ON car_models (year);
