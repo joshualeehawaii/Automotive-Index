b65dfad16718ab82a8ad87c74562bd876d9df3b0
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-07-22 20:09:34 -1000

    changes and new times for Nakaz

--- indexing.sql
+++ indexing.sql
@@ -1,26 +1,26 @@
 -- --Create a new postgres user named indexed_cars_user
-DROP DATABASE IF EXISTS indexed_cars;
-DROP USER IF EXISTS indexed_cars_user;
+-- DROP DATABASE IF EXISTS indexed_cars;
+-- DROP USER IF EXISTS indexed_cars_user;
 
 -- --Create a new database named indexed_cars owned by indexed_cars_user
-CREATE USER indexed_cars_user;
-CREATE DATABASE indexed_cars;
+-- CREATE USER indexed_cars_user;
+-- CREATE DATABASE indexed_cars;
 
 --Run the provided scripts/car_models.sql script on the indexed_cars database
 \c indexed_cars
 \i scripts/car_models.sql
 
 --Run the provided scripts/car_model_data.sql script on the indexed_cars database 10 times, there should be 223380 rows in car_models
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
-\i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
+-- \i scripts/car_model_data.sql
 
 --Run a query to get a list of all make_title values from the car_models table where the make_code is 'LAM',
 --without any duplicate rows, and note the time somewhere. (should have 1 result)
@@ -48,16 +48,13 @@ CREATE DATABASE indexed_cars;
 -- WHERE year = 2010; --Time: 51.104 ms
 
 SELECT DISTINCT make_title FROM car_models
-WHERE make_code = 'LAM'; --Time: 33.717 ms (OG time: 45.463 ms)
+WHERE make_code = 'LAM'; --Time: 2.6
 
 SELECT DISTINCT model_title FROM car_models
-WHERE make_code = 'NISSAN' AND model_code = 'GT-R'; --Time: 30.406 ms (OG time: 60.088 ms)
+WHERE make_code = 'NISSAN' AND model_code = 'GT-R'; --Time: 2.7
 
 SELECT * FROM car_models
-WHERE year >= 2010 AND year <= 2015; --Time: 99.006 ms (OG time: 145.176 ms)
+WHERE year >= 2010 AND year <= 2015; --Time: 97.0
 
 SELECT * FROM car_models
-WHERE year = 2010; --Time: 18.776 ms (OG time: 51.104 ms)
-
-
-
+WHERE year = 2010; --Time: 41.9
